2e808c6c96926a762646bf6e6c2c7bdf0b40f59f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-15 20:44:18 +0200

    Prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string with printf
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

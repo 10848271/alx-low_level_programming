84087db6ca4d53418ca8366d9a48e89523161dc9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-20 19:28:20 +0200

    Checks for lowercae letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+/**
+ * _isLower - checks for lowercase letters
+ * @c: character to check
+ * Return: 1 if lowercase or 0 if not lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}

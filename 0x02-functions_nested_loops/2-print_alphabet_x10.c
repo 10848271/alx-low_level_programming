eab4183dd58b808dafad23a4080775b66b3505f4
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-20 19:08:43 +0200

    Prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ * Return: noting
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		int i = 97;
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+		j++;
+		_putchar('\n');
+	}
+}

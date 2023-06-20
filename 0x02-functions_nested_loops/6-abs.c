17cc237b72660f607490cb6f33cd73025de4c117
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-20 21:25:02 +0200

    Computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+ * _abs - computes absolute value of an integer
+ * @n: number to be worked on
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-(n));
+	return (n);
+}

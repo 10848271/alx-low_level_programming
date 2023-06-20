7161ea0dbadff2f1b7ebe2b72590d4a10fc9ef5d
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-20 23:58:39 +0200

    Pprints all natural numbers from n to 98, followed by a new line

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting point of printing
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		if (n != 98)
+			printf("%d, ", n);
+		else
+			printf("%d\n", n);
+		n++;
+	}
+}

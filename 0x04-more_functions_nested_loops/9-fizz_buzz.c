95c87ffabde70eb23156fa8f7e45986872dac96d
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-23 00:57:46 +0200

    Perform FizzBuzz on 1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - perform fizzbuzz game
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i++ <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}

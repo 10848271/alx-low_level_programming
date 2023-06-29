1b0bac15093749e3468eb48fede74d7e6d9ba469
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-30 01:13:26 +0200

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,28 +5,18 @@
  * @s1: first string
  * @s2: second string
  * Return: 0 if s1 == s2
- *	   -15 if s1 < s2
- *	   15 if s1 > s2
+ *	   <0 if s1 < s2
+ *	   >0 if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int ret = 0; 
-	long unsigned int i = 0, len = strlen(s1);
+	int ret = 0, i = 0;
 
-	if (strlen(s2) > len)
-		len = strlen(s2);
-
-	for (i = 0; i < len; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] < s2[i])
-			return (-15);
-		else if (s1[i] > s2[i])
-			return (15);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	if (strlen(s1) > i)
-		return (15);
-	else if (strlen(s2) > i)
-		return (-15);
 
 	return (ret);
 }

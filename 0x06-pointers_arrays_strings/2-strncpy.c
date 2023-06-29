96f6fd94376f5bb6b527253f7060d3426dbd7b98
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-29 23:13:36 +0200

    Copies the string from source to destination

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strncpy - copies one string into the other based on a counter.
+ * @dest: where the string is to be copied to
+ * @src: the string to be copied
+ * @n: counter that specifies the number of characters to copy
+ * Return: resulting pointer of the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n ; i++)
+		dest[i] = src[i];
+	if (src[i] == '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}

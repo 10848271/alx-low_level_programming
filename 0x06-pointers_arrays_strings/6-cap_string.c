49b6e5cc565108235cb076d65152b87726302314
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-30 16:39:26 +0200

    Capitalize all worlds in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,13 @@ char *cap_string(char *ptr)
 	for (i = 0; ptr[i] != '\0'; i++)
 	{
 		if (ptr[i] == ' ' || ptr[i] == '\t' ||
-				ptr[i] == '\n' || ptr[i] == ',' 
+				ptr[i] == '\n' || ptr[i] == ','
 				|| ptr[i] == ';' ||
 				ptr[i] == '.' || ptr[i] == '!' ||
 				ptr[i] == '?' || ptr[i] == '"' ||
 				ptr[i] == '(' || ptr[i] == ')' ||
 				ptr[i] == '{' || ptr[i] == '}')
 		{
-			if (ptr[i] == '\t')
-				ptr[i] = ' ';
 			if (ptr[i + 1] > 96 && ptr[i + 1] < 123)
 			{
 				ptr[i + 1] = ptr[i + 1] - 32;

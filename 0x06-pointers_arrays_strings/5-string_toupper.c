08fafe0a71a459181156b1761aa8dc63ff768e42
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-30 15:44:47 +0200

    Change lowercase string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @ptr: pointer that holds the string
+ * Return: pointer to the altered string
+ */
+char *string_toupper(char *ptr)
+{
+	 int i = 0;
+
+	 const int upper = 32;
+
+	 while (ptr[i] != '\0')
+	 {
+		if (ptr[i] > 96 && ptr[i] < 123)
+		{
+			ptr[i] = (ptr[i] - upper);
+		}
+		i++;
+	 }
+
+	 return (ptr);
+}

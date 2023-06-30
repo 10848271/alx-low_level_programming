ab2a703af3254c7f038725b17ecae9eb17f058a4
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-06-30 16:22:31 +0200

    Capitalize all worlds in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+/**
+ * cap_string - capiltalizes all words of a string
+ * @ptr: pointer holding the string
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		if (ptr[i] == ' ' || ptr[i] == '\t' ||
+				ptr[i] == '\n' || ptr[i] == '\n' ||
+				ptr[i] == ',' || ptr[i] == ';' ||
+				ptr[i] == '.' || ptr[i] == '!' ||
+				ptr[i] == '?' || ptr[i] == '"' ||
+				ptr[i] == '(' || ptr[i] == ')' ||
+				ptr[i] == '{' || ptr[i] == '}')
+		{
+			if (ptr[i] == '\t')
+				ptr[i] = ' ';
+			if (ptr[i + 1] > 96 && ptr[i + 1] < 123)
+			{
+				ptr[i + 1] = ptr[i + 1] - 32;
+				i++;
+			}
+		}
+	}
+
+	return (ptr);
+}

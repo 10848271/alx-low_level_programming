0ebdef31efd68693894e214deca7948ed96e1561
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-07-04 01:26:53 +0200

    Function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+
+/**
+ * _strchr - find first occurence of a character
+ * @s: pointer to array
+ * @c: character to search
+ * Return: pointer to first occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ptr = NULL;
+
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	}
+
+	if (s[i] == c)
+		ptr = &s[i];
+
+	return (ptr);
+}

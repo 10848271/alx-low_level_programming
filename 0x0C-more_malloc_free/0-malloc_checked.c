b09ccfbe6b7309a626018b9d080acfcb341d4cdb
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-07-13 23:34:23 +0200

    Function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
  * malloc_checked - returns allocated memory
  * @b: byte size to allocate
  * Return: a pointer to allocated memory or
- * 	   exit with status 98 if allocation failed
+ *	   exit with status 98 if allocation failed
  */
 void *malloc_checked(unsigned int b)
 {

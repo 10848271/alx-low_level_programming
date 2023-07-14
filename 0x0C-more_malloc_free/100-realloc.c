ec3bb98e276d8d53646555b9ac2e183872d3778f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-07-14 02:03:10 +0200

    Reallocates memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: memory to reallocate
+ * @old_size: size of pointer to be reallocated
+ * @new_size: size of the reallocated memory
+ * Return: the reallocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr, *char_ptr;
+	unsigned int idx;
+	
+	if (!ptr)
+	{
+		free(ptr);
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size > old_size)
+	{
+		new_ptr = malloc(sizeof(char) * new_size);
+		char_ptr = ptr;
+		for (idx = 0; idx < old_size; idx++)
+			new_ptr[idx] = char_ptr[idx];
+		free(ptr);
+	}
+
+	return (ptr);
+}

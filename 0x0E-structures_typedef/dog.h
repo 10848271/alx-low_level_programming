ab4d0f7a388904dfa1115421f3084183355f4779
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-07-18 02:06:13 +0200

    Structs in C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog features
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Description: represents the features every dog has
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/*Function Prototypes*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif

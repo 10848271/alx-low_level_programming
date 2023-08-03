db097579df5b9ad0db688c830564a2ddfd1aba60
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-08-03 02:06:18 +0200

    Betty style corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 int _sqrt_helper(int n, int guess);
 /**
- * _sqrt_recursion - Returns the natural square root of a number using recursion
+ * _sqrt_recursion - Returns the natural square root
+ * of a number using recursion
  * @n: The number for which to find the square root
  *
  * Return: The natural square root of the number or -1 if it does not have
@@ -9,14 +10,15 @@ int _sqrt_helper(int n, int guess);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0 || n == 1)
-		return n;
-	return _sqrt_helper(n, 1);
+		return (n);
+	return (_sqrt_helper(n, 1));
 }
 
 /**
- * _sqrt_helper - Helper function to find the natural square root using recursion
+ * _sqrt_helper - Helper function to find
+ * the natural square root using recursion
  * @n: The number for which to find the square root
  * @guess: The current guess for the square root
  *
@@ -26,10 +28,8 @@ int _sqrt_recursion(int n)
 int _sqrt_helper(int n, int guess)
 {
 	if (guess * guess == n)
-		return guess;
+		return (guess);
 	if (guess * guess > n)
-		return -1;
-	
-	return _sqrt_helper(n, guess + 1);
+		return (-1);
+	return (_sqrt_helper(n, guess + 1));
 }
-
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,12 @@
 int is_prime_helper(int n, int divisor)
 {
-    if (n <= 1)
-        return 0;
-    if (n == divisor)
-        return 1;
-    if (n % divisor == 0)
-        return 0;
-
-    return is_prime_helper(n, divisor + 1);
+	if (n <= 1)
+		return (0);
+	if (n == divisor)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_helper(n, divisor + 1));
 }
 
 /**
@@ -18,6 +17,6 @@ int is_prime_helper(int n, int divisor)
  */
 int is_prime_number(int n)
 {
-    return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
 

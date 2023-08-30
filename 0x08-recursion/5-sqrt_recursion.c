d03cda36461365f651c192a5e08108f721e70f96
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-08-30 19:37:46 +0200

    Betty style compliance

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,20 @@
-int _sqrt_helper(int, int);
+/**
+ * _sqrt_helper - finds root of number using recursive
+ * @n: number to find root
+ * @dividend: number to test if its n's root
+ *
+ * Return: dividend or -1
+ */
+int _sqrt_helper(int n, int dividend)
+{
+        int root = dividend * dividend;
+
+        if (root == n)
+                return (dividend);
+        if (root > n)
+                return (-1);
+        return (_sqrt_helper(n, dividend + 1));
+}
 
 /**
  * int _sqrt_recursion - find natural root of a number
@@ -9,25 +25,8 @@ int _sqrt_helper(int, int);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0 || n == 1)
-		return n;
+		return (n);
 	return (_sqrt_helper(n, 1));
-}
-
-/**
- * _sqrt_helper - finds root of number using recursive
- * @n: number to find root
- * @dividend: number to test if its n's root
- *
- * Return: divided or -1
- */
-int _sqrt_helper(int n, int dividend)
-{
-	int root = dividend * dividend;
-	if (root == n)
-		return dividend;
-	if (root > n)
-		return -1;
-	return (_sqrt_helper(n, dividend + 1));
 }

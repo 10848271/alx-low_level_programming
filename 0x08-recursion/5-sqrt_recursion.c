d143be90101325cddd719975cb97a6eea32c149f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-08-30 18:21:36 +0200

    Calculate natural roots of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,32 @@
-int _sqrt_helper(int n, int guess);
+int _sqrt_helper(int, int);
+
 /**
- * _sqrt_recursion - Returns the natural square root
- * of a number using recursion
- * @n: The number for which to find the square root
+ * int _sqrt_recursion - find natural root of a number
+ * @n: number to find the natural root for
  *
- * Return: The natural square root of the number or -1 if it does not have
- *         a natural square root
+ * Return: natural root or -1
  */
 int _sqrt_recursion(int n)
 {
+	if (n == 0 && n == 1)
+		return n;
 	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
+		return -1;
 	return (_sqrt_helper(n, 1));
 }
 
 /**
- * _sqrt_helper - Helper function to find
- * the natural square root using recursion
- * @n: The number for which to find the square root
- * @guess: The current guess for the square root
+ * _sqrt_helper - finds natural root using recursive
+ * @n: number to find natural root
+ * @dividend: number to test if it is square root
  *
- * Return: The natural square root of the number or -1 if it does not have
- *         a natural square root
+ * Return: dividend or -1
  */
-int _sqrt_helper(int n, int guess)
+int _sqrt_helper(int n, int dividend)
 {
-	if (guess * guess == n)
-		return (guess);
-	if (guess * guess > n)
-		return (-1);
-	return (_sqrt_helper(n, guess + 1));
+	if (dividend * dividend == n)
+		return dividend;
+	if (dividend * dividend > n)
+		return -1;
+	return (_sqrt_helper(n, dividend + 1));
 }

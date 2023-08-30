cac9716a7d16073141f0c8f1b4dbbba70fc9d77b
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-08-30 19:04:33 +0200

    Calculate natural root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+int _sqrt_helper(int, int);
+
+/**
+ * int _sqrt_recursion - find natural root of a number
+ * @n: number to find root
+ *
+ * Return: natural root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return -1;
+	if (n == 0 || n == 1)
+		return n;
+	return (_sqrt_helper(n, 1));
+}
+
+/**
+ * _sqrt_helper - finds root of number using recursive
+ * @n: number to find root
+ * @dividend: number to test if its n's root
+ *
+ * Return: divided or -1
+ */
+int _sqrt_helper(int n, int dividend)
+{
+	int root = dividend * dividend;
+	if (root == n)
+		return dividend;
+	if (root > n)
+		return -1;
+	return (_sqrt_helper(n, dividend + 1));
+}

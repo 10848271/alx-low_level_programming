48c7c181205decf60278583e5d4a32d7a824c240
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-09-12 23:53:50 +0200

    Dog struct datatype

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - data about a dog
+ * @name: name of the dog struct instance
+ * @age: age of dog struct instance
+ * @owner: owner of dog struct instance
+ *
+ * Description: A structure data type used to represent a real world dog object
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

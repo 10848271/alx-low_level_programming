48c5555268aac922f5bc221702c693180ad5337f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-09-12 20:22:56 +0200

    Create an absolute function converter macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef __FUNCTION_LIKE_MACRO__
 #define __FUNCTION_LIKE_MACRO__
-#define ABS(x) ( (x) < 0 ? (-(x)) : (x) )
+#define ABS(x) ( ((x) < 0) ? (-(x)) : (x) )
 #endif /* end ifndef __FUNCTION_LIKE_MACRO__ */

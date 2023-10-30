e756fb2d2b2ad5f7fdd5b64de72bc8a1f3fd91c9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-10-30 22:06:33 +0100

    Solve problem

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,20 @@ char *str_concat(char *s1, char *s2)
 	int l1, l2, len, idx;
 	char *c_str;
 
+	if (!l1)
+		return (l2);
+	if (!l2)
+		return (l1);
+	if (!l1 && !l2)
+		return ("");
 	for (l1 = 0; s1[l1]; l1++)
 		continue;
 	for (l2 = 0; s2[l2]; l2++)
 		continue;
 	len = l1 + l2;
 	c_str = malloc((sizeof(char) * len) + 1);
+	if (!c_str)
+		return (NULL);
 	for (idx = 0; idx < l1; idx++)
 		c_str[idx] = s1[idx];
 	for (l1 = 0; idx < len; idx++, l1++)

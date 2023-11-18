c83b1696296239c68dec960eb5211bdb87c0532c
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-18 08:43:24 +0100

    Counts elements in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - counts the number of elements in a linked list
+ * @h: head of linked list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	const *trav;
+
+	if (h == NULL)
+		return (len);
+	trav = h;
+	while (trav != NULL)
+	{
+		len++;
+		trav = trav->next;
+	}
+
+	return (len);
+}

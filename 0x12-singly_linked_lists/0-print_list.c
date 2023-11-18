052e80e179d03550a5280256d728330a45c79666
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-18 08:17:42 +0100

    Linked list traversal to prints content and return number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints a linked list
+ * h: head node of linked list
+ *
+ * Return: number of nodes in linked lists
+ */
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+	const list_t *trav;
+
+	if (h == NULL)
+		return (len);
+	trav = h;
+	while (trav != NULL)
+	{
+		if (trav->str != NULL)
+			printf("[%d] %s\n", trav->len, trav->str);
+		else
+			printf("[0] (nil)\n");
+		len++;
+		trav = trav->next;
+	}
+
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# 0x12. C - Singly linked lists
+
+Linked lists data structure concepts implemented in C

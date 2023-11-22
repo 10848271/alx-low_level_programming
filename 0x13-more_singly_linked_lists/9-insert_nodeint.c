e4e884686f64beff85346d3309fec8f1ba0e8ab0
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-11-22 01:45:20 +0100

    Able to add node at position 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	trav = *head;
 	while (trav != NULL)
 	{
-		if (count == idx - 1)
+		if (count == idx - 1 || idx == 0)
 			break;
 		trav = trav->next;
 		count++;
@@ -29,6 +29,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+
+		return (*head);
+	}
+
 	new->n = n;
 	new->next = trav->next;
 	trav->next = new;
